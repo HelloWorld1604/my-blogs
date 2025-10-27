759983f0d367c4e9bbbc7cb7e1ae6e9296623e76
HelloWorld1604 <hoshizon65@gmail.com>
2025-10-27 10:19:40 +0900

    Sửa responsive của Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div id="home">
             <p id="Title">Hello World</p>
-            <p style={{fontFamily:"Inter", color:"#404040", fontSize:"20px"}}>welcome to my landing page</p>
+            <p id="Title-sub-text">welcome to my landing page</p>
             <div className="home-buttons">
                 <a href="#about-me" id="about_me_button">About me</a>
                 <a href="#about-me" id="blogs_button">Blogs</a>
