954b5a0504023d5544d910e2c290e0adc041fb78
HelloWorld1604 <hoshizon65@gmail.com>
2026-02-02 12:07:24 +0900

    Style Blog-Detail page

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -1,3 +1,5 @@
+import "./BlogDetail.css";
+
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -22,13 +24,15 @@ function BlogDetail() {
     <div>
         <Navigation/>
         <div>
-            <article>
-            <img  src={import.meta.env.BASE_URL + post.illustration}
-                  style={{ width:"30vw", height:"60vh"}}/>
-            <h1>{post.title}</h1>
-            <small>{post.created_at}</small>
-            <hr />
-            <p>{post.content}</p>
+            <article className="post-container">
+              <h3 className="post-hastag">#{post.id}</h3>
+              <h1 className="post-title">{post.title}</h1>
+              <small className="post-update">{post.created_at}</small>
+              <img className="post-illu"  src={import.meta.env.BASE_URL + post.illustration}/>
+              <div className="post-line" />
+              <div className="post-content-wrap">
+                <p className="post-content">{post.content}</p>
+              </div>
             </article>
         </div>
     </div>
